1e819dbe394827342e108cbee800210ed933cc89
Federico Angelo Mor <66522155+federicomor@users.noreply.github.com>
2022-01-13 18:18:14 +0100

    Update frenet.jl

--- frenet.jl
+++ frenet.jl
@@ -9,12 +9,12 @@ t = range(0, 6pi, length=n)
 f(t) = t*cos(t)
 g(t) = t*sin(t)
 h(t) = t
-molt=2.0
+molt = 2.0
 # Derivatives of f,g,h
 f1(t)=cos(t)-t*sin(t)
-f2(t)=-sin(t)-sin(t)-t*cos(t)
+f2(t)=-2sin(t)-t*cos(t)
 g1(t)=sin(t)+t*cos(t)
-g2(t)=cos(t)+cos(t)-t*sin(t)
+g2(t)=2cos(t)-t*sin(t)
 h1(t)=1
 h2(t)=0
 
@@ -22,10 +22,10 @@ x=f.(t)
 y=g.(t)
 z=h.(t)
 anim=@animate for i in 1:length(t)
-    if i%20==0
-        println(100i/length(t),"%")
+    if i%(n/10)==0
+        print(Int64(100i/length(t)),"% -> ")
     end
-    p = plot(x, y, z,legend=false)
+    p = plot(x, y, z,legend=false, aspect_ratio=:equal)
     draw_trihedron(t[i], i, molt)
     # uncomment to oscillate the camera angle (values in degrees: (azimuth, altitude))
     plot!(p, camera = (i*45/length(t), 40))
